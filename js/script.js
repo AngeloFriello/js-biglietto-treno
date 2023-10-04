3f70cebfacbf007d61c242e83d896bef4700135e
angelo friello <angelofriello01@gmail.com>
2023-10-04 15:34:19 +0200

    add js prezzo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
 // # calcolare il prezzo del biglietto del treno
 // - chiedere al passeggero quanti anni ha
+let età = prompt ('quanti anni hai?')
+console.log(età)
 // - chiedere al passeggero quanti km vuole percorrere
-let kmTotali = prompt ('quanti km vuoi percorrere')
+let kmTotali = prompt('quanti km vuoi percorrere?')
 // - definire il prezzo al km base di (0.21$ al km)
+let km = '0.21'
 
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65
+if (età < 18) {
+
+    km = ((km * 80) / 100 )
+
+}else if (età > 65) {
+
+    km = ((km * 60) / 100 )
+
+}else {
+    km = '0.21'
+}
+let prezzoTotale = (km * kmTotali)
+console.log(prezzoTotale)
+
 // - far comparire il prezzo finale con lo sconto aplicato
+
+let firstPartMessage 
+
+
+if (età < 18) {
+
+    firstPartMessage = ('questo è il prezzo del tuo biglietto con sconto aplicato')
+
+}else if (età > 65) {
+
+    firstPartMessage = ('questo è il prezzo del tuo biglietto con sconto aplicato')
+
+}else {
+    firstPartMessage = ('questo è il prezzo del tuo biglietto')
+}
+
+const messageDomElement = document.getElementById('prezzo');
+messageDomElement.innerHTML = firstPartMessage + (': ') + prezzoTotale + ('$')  ;
